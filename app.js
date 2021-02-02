cdcdff168a49cf84ee444c8484b8b448dc4673ec
youminji62 <youminji62@gmail.com>
2021-02-02 23:04:18 +0900

    test

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/',(req,res) => res.send('Hello, Nodejs! code update, heroku testing'));
-//app.get('/welcome', (req,res) => res.send('Hi, there. welcome to the Nodejs service.'));
+app.get('/welcome', (req,res) => res.send('Hi, there. welcome to the Nodejs service.'));
 //app.get('/check', (req,res) => res.send('Example app listening on port 3000'));
 app.listen(port,()=>console.log('Example app listening on port 3000'))
